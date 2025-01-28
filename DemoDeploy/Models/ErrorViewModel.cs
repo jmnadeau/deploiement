ab406023e05f4ef3c64dfaad576dcebb64392b82
Jean-Michel Nadeau <jean-michel.nadeau@cegepmontpetit.ca>
2025-01-28 14:38:58 -0500

    Init

---
+++ DemoDeploy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoDeploy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
